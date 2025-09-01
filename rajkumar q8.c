e03349c70a34a4289d53857ea83835ca8f8707ba
rajkumar20071007-cpu <rajkumar20071007@gmail.com>
2025-09-01 10:12:50 +0530

    Add files via upload

---
+++ rajkumar q8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int num;
+	printf("enter an integer number:");
+	scanf("%d",&num);
+	printf("% 0 is octal of %d",num,num);
+	return 0;
+}
---
+++ rajkumar q9.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int num;
+	printf("enter a hexadecimal:");
+	scanf("%x",&num);
+	printf("%d is integer of num,);
+	return 0;
+}
